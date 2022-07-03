9d85828e046fbde5b1a42a5305560556938b2dc4
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2022-07-02 21:49:54 -0300

    Finalização do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoAng7';
+  public mode = 'list';
+  public todos: Todo[] = [];
+  public title: string = 'Minhas Tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });
+
+    this.load();
+  }
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+    this.save();
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+    this.save();
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+    this.save();
+  }
+
+  clear() {
+    this.form.reset();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    this.changeMode('list')
+  }
+
+  load() {
+    const data = localStorage.getItem('todos')
+    if (data) {
+      this.todos = JSON.parse(data)
+    } else {
+      this.todos = [];
+    }
+  }
+
+  changeMode(modo: string) {
+    this.mode = modo;
+  }
 }
